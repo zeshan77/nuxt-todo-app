3092552f6787c86cebd86f0a3acd93ec970deceb
Zeshan Khattak <zeshan77@gmail.com>
2020-11-19 17:55:25 +0500

    todo app in nuxtjs

---
+++ components/ToDos.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+        <ul class="d-block w-100 px-5">
+          <li v-for="(todo, index) in todos" :key="index" class="d-flex justify-content-between my-3 border-bottom pb-1">
+            <div>{{ todo.title}}</div>
+            <div>
+                <input v-if="todo.completed" type="checkbox" checked @click="toggleCompleted(todo)">
+                <input v-if="!todo.completed" type="checkbox" @click="toggleCompleted(todo)">
+            </div>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+        <div class="row px-5 mt-4">
+            <strong>Total todos: &nbsp; </strong> {{ todos.length - completedTodos + '/' + todos.length }}
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: [
+                
+            ]
+        }
+    },
+
+    async fetch() {
+        this.todos = [
+            {
+                    title: 'Drop kids to school',
+                    completed: true
+                },
+                {
+                    title: 'Grocery',
+                    completed: false
+                },
+                {
+                    title: 'Visit doctor',
+                    completed: false
+                },
+                {
+                    title: 'Call to mom',
+                    completed: false
+                }
+        ]
+    },
+
+    computed: {
+        completedTodos() {
+            return this.todos.filter(todo => todo.completed).length
+        }
+    },
+
+    methods: {
+        toggleCompleted(todo) {
+            let todoIndex = this.todos.indexOf(todo)
+            this.todos[todoIndex].completed = this.todos[todoIndex].completed ? false : true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        To Do App
+      </h1>
+      <to-dos />
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDos from '../components/ToDos.vue'
+export default {
+  components: { ToDos },
+  head: {
+    title: 'To Do App'
+  }
+}
+</script>
+
+<style>
+.container {
+  padding-top: 20px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
